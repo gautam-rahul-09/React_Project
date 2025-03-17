77375c2fbfc89ffec912b6fe13c36aa577ed441f
manish pandey <mpandey412@rku.ac.in>
2025-03-17 21:56:13 +0530

    first

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 const Home = () => {
-    return(
-        <div>
-        <h1>Home Page</h1>
-        <p>Welcome to the Home Page</p>
-        </div>
-    )
-}
-
-export default Home;
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900">
+        <h1 className="text-4xl font-bold text-blue-600">Home Page</h1>
+        <p className="mt-4 text-lg text-gray-700">Welcome to the Home Page</p>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
